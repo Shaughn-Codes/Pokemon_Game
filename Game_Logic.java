8ad61ebc4aef71032d0a8cf25cca1d51112d7987
Shaughn-Codes <shaughn.p.bulgar@gmail.com>
2021-04-30 17:12:21 -0400

    Test Upload

--- Game_Logic.java
+++ Game_Logic.java
@@ -6,6 +6,10 @@ public class Game_Logic {
     }
 
     public void waitforCommand(){
+
+        if (GameObjects.pc.inRoom == 0){
+            createCharacter();
+        }
         System.out.println("What to do?");
         Scanner sc  = new Scanner(System.in);
         String com = sc.nextLine();
@@ -19,6 +23,18 @@ public class Game_Logic {
 
     public void processCommand(String[] words){
 
+
+    }
+
+    public void createCharacter(){
+        System.out.println("Welcome to the gaem. What is your naem?");
+        Scanner sc = new Scanner(System.in);
+
+        Game_Objects.pc.name = sc.next();
+        System.out.println();
+        Game_Objects.po.hp = 100;
+        Game_Objects.pc.accuracy = 75;
+        Game_Objects.pc.inRoom = 2;
 
     }
 }
