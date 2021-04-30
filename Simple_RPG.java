d4d7165ae72371668ca8294e22b7d37e7894c5f7
simie <simeon.karakatsiotis67@gmail.com>
2021-04-30 16:52:54 -0400

    adding simple rpg class loop

---
+++ Simple_RPG.java
@@ -0,0 +1,15 @@
+public class Simple_RPG
+{
+    public static void main(string[] args)
+    {
+        while(true)
+        {
+            game_loop();
+        }
+    }
+    public static void game_loop()
+    {
+        
+    }
+
+}
