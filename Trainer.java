f70dee0f8279a5cc8f62ba18f4d14b3ae286445c
Shaughn-Codes <shaughn.p.bulgar@gmail.com>
2021-05-04 21:57:36 -0400

    Trainer update

--- Trainer.java
+++ Trainer.java
@@ -2,16 +2,22 @@ package Pokemon_Game;
 import java.util.Scanner;
 
 public class Trainer {
-    public static void main(String[] args) {
+
+
+    String name,pokemon,experience;
+
+    public static void main(String[] args) throws Exception {
+        Trainer.displayTrainer();
+
 
     }
 
-    String name,pokemon,experience;
 
     public Trainer(String name, String Pokemon, String Experience){
         this.name = name;
         this.pokemon = Pokemon;
         this.experience = Experience;
+        
 
     }
 
@@ -23,23 +29,24 @@ public class Trainer {
 
     public static void displayTrainer() throws Exception{
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello welcome to Pokemon!");
         System.out.println("Please choose a Trainer: Ash, Whitney, Brock");
-        String Trainer = scanner.nextLine();
+        String Trainer = scanner.next();
 
-        if (Trainer == "Ash" || Trainer == "ash"){
+        if (Trainer.equals("Ash") || Trainer.equals("ash")){
             Trainer Ash = new Trainer("Ash","Pikachu","Expert");
 
             System.out.println("You choose " + Ash.getName() + " as your Trainer!, he is equipped with the pokemon " + Ash.getPokemon() +
                     " and has an experience of " + Ash.getExperience()+ "!");
-        } else if (Trainer == "Whitney" || Trainer == "whitney"){
+        } else if (Trainer.equals("Whitney")|| Trainer.equals("whitney")){
             Trainer Whitney = new Trainer("Whitey","Miltank","Intermediate");
 
             System.out.println("You choose " + Whitney.getName() +  " as your Trainer!, she is equipped with the pokemon "+ Whitney.getPokemon() +
                     " and has an experience of " + Whitney.getExperience() + "!");
-        } else if (Trainer == "Brock" || Trainer == "brock"){
+        } else if (Trainer.equals("Brock") || Trainer.equals("brock")){
             Trainer Brock = new Trainer("Brock","Onix","Mid");
 
-            System.out.println("You choose " + Brock.getName() + " as your Trainer!, he is equipped with the Pokemon " + Brock.getName() +
+            System.out.println("You choose " + Brock.getName() + " as your Trainer!, he is equipped with the Pokemon " + Brock.getPokemon() +
                     " and he has a experience of " + Brock.getExperience() + "!");
         } else {
             throw new Exception("Please type in the given options!");
