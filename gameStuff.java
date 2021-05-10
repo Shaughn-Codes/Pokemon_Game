a367867fa3c8f8a412e918662ca2a6035f51b641
simie <simeon.karakatsiotis67@gmail.com>
2021-05-10 19:56:41 -0400

    implemented Commens Simeon Side

--- gameStuff.java
+++ gameStuff.java
@@ -1,4 +1,5 @@
 public class gameStuff {
+    /** Here is the refrence values to create new instances of the trainer class, Pokemon Pet class, and the jerry trainer himself. */
     public static trainer player = new trainer();
     public static PokemonPet pet = new PokemonPet();
     public static trainer jerry = new trainer();
